1eaa44ca988711838206b32632d7d989ef37bc1b
sanjeev kumar <sanjeevpat7@gmail.com>
2024-07-06 14:43:23 +0530

    updated_provider

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -1,17 +1,16 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended in production
-    }    
+      version = "3.111.0"
+    }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
 
 # Create Resource Group 
